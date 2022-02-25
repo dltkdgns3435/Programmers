82cf39dd16cf280cd1db8b2abd3ef57db33a116c
sh-mac <dltkdgns3435@naver.com>
2022-02-25 23:10:41 +0900

    Solved

---
+++ level_1/로또의_최고_순위와_최저_순위.java
@@ -0,0 +1,33 @@
+/**
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/77484
+ * 채점 결과
+ * 정확성: 100.0
+ * 합계: 100.0 / 100.0
+ */
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+
+        int zeroCnt = 0;
+        int matchCnt = 0;
+
+        for(int i=0; i<6; i++){
+            if(lottos[i] == 0){
+                zeroCnt++;
+            }
+            else{
+                for(int j=0; j<6; j++){
+                    if(lottos[i] == win_nums[j]){
+                        matchCnt++;
+                    }
+                }
+            }
+
+        }
+        int min = Math.min(7 - matchCnt, 6);
+        int max = Math.max(min - zeroCnt, 1);
+
+
+        return new int[]{max, min};
+    }
+}
