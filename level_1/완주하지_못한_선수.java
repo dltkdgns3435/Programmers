cfb361d2409bf9584aaf7aae4f70343c3632a841
sh-mac <dltkdgns3435@naver.com>
2022-02-26 23:09:42 +0900

    Solved

--- level_1/완주하지_못한_선수.java
+++ level_1/완주하지_못한_선수.java
@@ -1,11 +1,21 @@
 /**
- * 링크 : https://programmers.co.kr/learn/courses/30/lessons/42888
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/42576
  * 채점 결과
  */
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        return answer;
+        PriorityQueue<String> participantQueue = new PriorityQueue<>(Arrays.asList(participant));
+        PriorityQueue<String> completionQueue = new PriorityQueue<>(Arrays.asList(completion));
+
+        for (int i = 0; i < participant.length; i++) {
+            String p = participantQueue.poll();
+            String c = completionQueue.poll();
+            if(!p.equals(c)) return p;
+        }
+        return "";
     }
 }
