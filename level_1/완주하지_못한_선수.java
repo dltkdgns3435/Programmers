6b15550cd5003dea1ab6a3c18b2d85a96c30b641
sh-mac <dltkdgns3435@naver.com>
2022-02-26 22:58:18 +0900

    Not yet

--- level_1/완주하지_못한_선수.java
+++ level_1/완주하지_못한_선수.java
@@ -1,14 +1,11 @@
 /**
  * 링크 : https://programmers.co.kr/learn/courses/30/lessons/42888
  * 채점 결과
- * 정확성: 100.0
- * 합계: 100.0 / 100.0
  */
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        List<String>
         return answer;
     }
 }
